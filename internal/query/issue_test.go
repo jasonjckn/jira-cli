acc7b911a7c5ac298e518b5f0518feab6c955009
Ankit Pokhrel <ankitpokhrel@gmail.com>
2022-03-05 12:49:40 +0100

    ci: Make linter happy

--- internal/query/issue_test.go
+++ internal/query/issue_test.go
@@ -109,9 +109,9 @@ func (tfp issueFlagParser) GetStringArray(name string) ([]string, error) {
 	return tfp.labels, nil
 }
 
-func (tfp issueFlagParser) GetStringToString(string) (map[string]string, error) { return nil, nil }
-func (tfp issueFlagParser) GetUint(string) (uint, error)                        { return 100, nil }
-func (tfp issueFlagParser) Set(string, string) error                            { return nil }
+func (issueFlagParser) GetStringToString(string) (map[string]string, error) { return nil, nil }
+func (issueFlagParser) GetUint(string) (uint, error)                        { return 100, nil }
+func (issueFlagParser) Set(string, string) error                            { return nil }
 
 func TestIssueGet(t *testing.T) {
 	cases := []struct {
